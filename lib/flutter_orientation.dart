b31a4a2978161ed9963b1028f9c20ed21ef28cbf
chaves <mail@chavesgu.com>
2020-09-19 19:13:34 +0800

    :bug: fix

--- lib/flutter_orientation.dart
+++ lib/flutter_orientation.dart
@@ -1,11 +1,20 @@
 
+import 'dart:io';
+
 import 'package:flutter/services.dart';
 
 class FlutterOrientation {
   static const MethodChannel _channel =
       const MethodChannel('chavesgu/orientation');
 
-  static void setOrientation(DeviceOrientation orientation) {
-    _channel.invokeMethod('setOrientation', orientation.toString());
+  static Future<void> setOrientation(DeviceOrientation orientation) async {
+    try {
+      if (Platform.isIOS) {
+        await SystemChrome.setPreferredOrientations([orientation]);
+      }
+      await _channel.invokeMethod('setOrientation', orientation.toString());
+    } catch (e) {
+      //
+    }
   }
 }
